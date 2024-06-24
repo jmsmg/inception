24465b6984949de44baef4e45ec373e1dda2a293
jmsmg <jmsmg1@me.com>
2024-06-24 11:17:41 +0900

    mariadb ok

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e
+set -e
 
 service mariadb start
 
@@ -8,4 +8,15 @@ mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-# tail -f /dev/null
+service mariadb stop
+
+# chmod +x /usr/sbin/mariadbd 
+
+# rm -f /var/lib/mysql/aria_log_control*
+
+/usr/sbin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --skip-log-error --pid-file=/run/mysqld/mysqld.pid --socket=/run/mysqld/mysqld.sock --bind-address=0.0.0.0
+
+# tail -f
+
+# TODO
+# 연결 되는지 쏴봐야함
